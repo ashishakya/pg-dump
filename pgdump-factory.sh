185d7039438372da87976687603ad4c85b8b68b4
Ashish Shakya <ashishakya@outlook.com>
2020-03-05 22:15:49 +0545

    PGD-3 Relate pgdump factory to individual script file
    Closes: #3

--- pgdump-factory.sh
+++ pgdump-factory.sh
@@ -13,17 +13,17 @@ select opt in "${options[@]}"
 do
 	case $opt in
 		"PROJECT 1")
-			#./pgdump-scripts
+			# ./pgdump-scripts/{Enter the name of script for your project 1}
 			echo 'you selected project 1'
 			break
 			;;
 		"PROJECT 2")
-			# ./pgdump-scripts
+			# ./pgdump-scripts/{Enter the name of script for your project 2}
 			echo 'you selected project 2'
 			break
 			;;
 		"PROJECT 3")
-			# ./pgdump-scripts
+			# ./pgdump-scripts/{Enter the name of script for your project 2}
 			echo 'you selected project 3'
 			break
 			;;
